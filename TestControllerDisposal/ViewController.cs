80937efc1a736ea3528127b3dc651cb6e6b58f78
Sergey Nesteruk <dj_decibell@mail.ru>
2017-03-30 18:25:16 +0300

    Initial commit

---
+++ TestControllerDisposal/ViewController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UIKit;
+
+namespace TestControllerDisposal
+{
+	public partial class ViewController : UIViewController
+	{
+		UIScrollView scroll;
+
+		protected ViewController(IntPtr handle) : base(handle)
+		{
+			// Note: this .ctor should not contain any initialization logic.
+		}
+
+		public override void ViewDidLoad()
+		{
+			base.ViewDidLoad();
+			scroll = new UIScrollView();
+			scroll.WeakDelegate = this;
+			//View.AddSubview(scroll);
+		}
+
+		protected override void Dispose(bool disposing)
+		{
+			base.Dispose(disposing);
+			Console.WriteLine("Disposing View Controller");
+		}
+	}
+}
